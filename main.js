129bf310c7f9bd5d30623207d59d37c8c3fa2785
Andre K <andrek@Andres-MacBook-Pro.local>
2022-07-10 05:05:48 -0400

    1st commit

--- main.js
+++ main.js
@@ -1,5 +1,102 @@
 // Your code here.
 
+/*
+getFirstName()
+
+parameters 1 = object
+{
+    firstName: string,
+    lastName: string,
+}
+
+getfirstName
+*/
+
+function getFirstName(person){
+  return person.firstName;
+}
+
+console.log(getFirstName({firstName: 'Colin' , lastName:'Jaffe'})) // -> 'Colin'
+console.log(getFirstName({firstName: 'Petra', lastName:'Solano'})) // -> 'Petra'
+
+// getLastName
+
+function getLastName (person){
+  return person.lastName
+}
+
+// getFullName
+
+function getFullName (person){
+  return `${person.firstName} ${person.lastName}`
+}
+
+/*
+
+parameters: 2
+1st parameter
+person = {
+  firstName: string, 
+  LastName: string 
+}
+newFirstName: string
+
+change firstName of person object to newFirstName
+*/
+
+// setFirstName 
+
+function setFirstName(person, newFirstName){
+  //person['firstName'] = newFirstName;
+  person.firstName  = newFirstName;
+}
+
+let person1 = {firstName: 'Colin', lastName: 'Jaffe'};
+let person2 = {firstName: 'Petra', lastName: 'Solano'};
+setFirstName(person1, 'Redacted');
+setFirstName(person2, 'Anthony');
+
+console.log(person1);
+console.log(person2);
+
+// setAge
+
+function setAge (age, person) {
+  person.age = age
+}
+
+
+// giveBirthday
+
+function giveBirthday (person) {
+  if(person.age >= 0){
+    person.age += 1
+  } else person.age =1 
+}
+
+// marry
+
+function marry (per1, per2) {
+  per1.married = true
+  per2.married = true
+
+  per2.spouseName = per1.firstName + "" + per1.lastName
+  per1.spouseName = per2.firstName + "" + per2.lastName
+
+}
+
+// divorce
+
+function divorce(per1, per2){
+  per1.married = false
+  per2.married = false
+
+  delete per1.spouseName
+  delete per2.spouseName
+}
+
+
+
 
 
 
